1acf8fe1ab43c7b9bcb9a59732c9c8884d8e5a8e
louispeters <46196893+louispeters@users.noreply.github.com>
2019-01-06 15:50:19 +0000

    Update getplayers.py

--- getplayers.py
+++ getplayers.py
@@ -1,4 +1,8 @@
 numberplayers=int(input("How Many Players?"))
 print("Number of players is:"+str(numberplayers))
-player_list = [numberplayers]
-player_list 
+player_list = []
+for x in range (0,numberplayers):
+    player_list.append(input("what is the name of player "+str(x + 1)+"?"))
+    #this is a comment and can be deleted
+for x in range (0,numberplayers):
+    print("name of player "+ str(x + 1) +" is " + player_list[x])
